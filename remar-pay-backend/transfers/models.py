93f87a50c1d2cb2a509fd5ab2077d3c22ec751f2
emjaay2900 <mjdederi01@gmail.com>
2025-06-27 16:45:26 +0100

    Trasfer app initiated and modelsdefined

---
+++ remar-pay-backend/transfers/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+
+# Create your models here.
+from django.db import models
+from django.conf import settings
+
+class FundTransfer(models.Model):
+    CURRENCY_CHOICES = [
+        ('NGN', 'Nigerian Naira'),
+        ('XOF', 'West African CFA (Niger)'),
+        ('XAF', 'Central African CFA (Cameroon)'),
+    ]
+
+    sender = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='sent_transfers')  # Chief Manager
+    recipient = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='received_transfers')  # Agent
+    currency = models.CharField(max_length=5, choices=CURRENCY_CHOICES)
+    amount = models.DecimalField(max_digits=12, decimal_places=2)
+    note = models.TextField(blank=True, null=True)
+    is_confirmed = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    confirmed_at = models.DateTimeField(null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.sender.name} → {self.recipient.name} | {self.amount} {self.currency}"
