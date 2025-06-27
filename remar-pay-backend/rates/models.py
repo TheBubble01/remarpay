51b823b5e9faa19fb94ad8173e5a667afd69719b
emjaay2900 <mjdederi01@gmail.com>
2025-06-27 10:18:19 +0100

    rates models defined

--- remar-pay-backend/rates/models.py
+++ remar-pay-backend/rates/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+from django.utils import timezone
+
+class ExchangeRate(models.Model):
+    COUNTRY_CHOICES = [
+        ('nigeria', 'Nigeria'),
+        ('niger', 'Niger'),
+        ('cameroon', 'Cameroon'),
+    ]
+
+    country = models.CharField(max_length=20, choices=COUNTRY_CHOICES)
+    market_rate = models.DecimalField(max_digits=10, decimal_places=2)
+    remar_rate = models.DecimalField(max_digits=10, decimal_places=2)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.country.upper()} - Remar Rate: {self.remar_rate}"
