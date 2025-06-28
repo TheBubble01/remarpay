1ac7b19e697618ef2160eaeb5128b8e42aff1187
emjaay2900 <mjdederi01@gmail.com>
2025-06-28 12:14:07 +0100

    Added functions for assigning roles and countries by manager or tech-admin

--- remar-pay-backend/accounts/serializers.py
+++ remar-pay-backend/accounts/serializers.py
@@ -1,7 +1,6 @@
 from rest_framework import serializers
 from .models import User
 from django.contrib.auth.password_validation import validate_password
-from rest_framework import serializers
 
 class UserCreateSerializer(serializers.ModelSerializer):
     # Confirm password field (optional, for UX)
